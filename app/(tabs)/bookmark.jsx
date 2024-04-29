81d7ef982e07c7cfd2f82f78f1a55a7805154f9b
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2024-04-28 21:43:54 -0500

    new updates

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,10 +1,135 @@
-import { Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GoogleGenerativeAI } from "@google/generative-ai"
+import { API_KEY } from "@env"
+import { useRef, useState, useEffect } from "react";
+import { getAiGeneratedPlan, getCustomDentalPlan, updateAiGeneratedPlan } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { CustomButton, Loader } from "../../components";
+import Markdown from "react-native-markdown-display";
+
+
 
 const Bookmark = () => {
+
+  const { user } = useGlobalContext();
+
+
+  const ref = useRef(null)
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [isLoading, setLoading] = useState(true)
+
+  const [text, setText] = useState(null)
+
+  const genAI = new GoogleGenerativeAI(API_KEY);
+
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+
+  useEffect(() => {
+    async function run() {
+      const plan = await getCustomDentalPlan(user.$id);
+
+      const {
+        age,
+        existingConditions,
+        recentProcedures,
+        hygieneFrequency,
+        toothbrush,
+        toothpaste,
+        dietaryHabits,
+        dentalAppliances_allergies,
+        creator_dental
+      } = plan
+    }
+
+    run();
+  }, []); // Empty dependency array ensures it runs only once
+
+  useEffect(() => {
+
+
+      getAiGeneratedPlan(user).then(res => {
+        setText(res.ai_generated_plan == "" ? null : res.ai_generated_plan)
+        setLoading(false)
+      })
+
+   
+
+
+  }, [user])
+
+
+  const handleSubmit = async () => {
+    setSubmitting(true)
+    const plan = await getCustomDentalPlan(user.$id);
+
+    const {
+      age,
+      existingConditions,
+      recentProcedures,
+      hygieneFrequency,
+      toothbrush,
+      toothpaste,
+      dietaryHabits,
+      dentalAppliances_allergies,
+      creator_dental
+    } = plan
+
+    console.log(plan)
+
+    const prompt = `Using the following information, creating a custom dental plan: \n age: ${age}, \n Do you have any existing dental conditions or concerns?: ${existingConditions} \n
+    Did you have any recent dental procedures or discomfort?: ${recentProcedures} \n
+    How often do you brush, floss, and use mouthwash?: ${hygieneFrequency} \n
+    What type of toothbrush do you use?: ${toothbrush} \n
+    What type of toothpaste do you use?: ${toothpaste} \n
+    What dietary habits or health concerning conditions/habits do you have? (ex: sugary, acidic foods, smoking): ${dietaryHabits} \n
+    What dental appliances or allergies do you have?: ${dentalAppliances_allergies}
+    `
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+
+    setText(text)
+    setSubmitting(false)
+
+    await updateAiGeneratedPlan(user, text)
+  }
+
+  if(isLoading) {
+    return (
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView className="px-4 my-6">
+          <Text className="text-2xl text-white font-psemibold">Loading...</Text>
+          <Loader isLoading={isLoading}/>
+        </ScrollView>
+      </SafeAreaView>
+    )
+  }
+
   return (
     <SafeAreaView className="px-4 my-6 bg-primary h-full">
-      <Text className="text-2xl text-white font-psemibold">Bookmark</Text>
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Bookmark</Text>
+
+        <View className="flex justify-center items-center px-4">
+          <Text className="text-xl text-white font-psemibold mt-7" ref={ref}>
+            {text}
+
+          </Text>
+
+          <CustomButton
+            title={"Create AI Generated Dental Plan from your Dental History"}
+            handlePress={handleSubmit}
+            containerStyles="mt-7 p-5 w-full"
+            textStyles={""}
+            isLoading={isSubmitting}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- app/(tabs)/dental.jsx
+++ app/(tabs)/dental.jsx
@@ -1,45 +1,54 @@
-import { router } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Image, FlatList, TouchableOpacity, Text, ScrollView, Alert } from "react-native";
-
-import { icons } from "../../constants";
-import useAppwrite from "../../lib/useAppwrite";
-import { createCustomDentalPlan, getUserPosts, signOut, updateCustomDentalPlan } from "../../lib/appwrite";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView, Text, Alert, KeyboardAvoidingView, Platform } from "react-native";
+import { DentalHistoryFormField, CustomButton, Loader } from "../../components";
+import { getCustomDentalPlan, updateCustomDentalPlan } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
-import { CustomButton, EmptyState, FormField, InfoBox, VideoCard } from "../../components";
-import { DentalHistoryFormField } from "../../components";
-import { useState } from "react";
-
 
 const DentalHistory = () => {
-
-  const { setUser, setIsLogged, setNewUser, newUser, user } = useGlobalContext();
-  const [isSubmitting, setSubmitting] = useState(false);
-
+  const { user } = useGlobalContext();
   const [form, setForm] = useState({
-    // title: "",
-    age: null,
+    age: "",
     existingConditions: "",
     recentProcedures: "",
     hygieneFrequency: "",
     toothbrush: "",
     toothpaste: "",
     dietaryHabits: "",
-    // dentalAppliances_allergies: "h",
+    dentalAppliances_allergies: "",
   });
+  const [isLoading, setLoading] = useState(true);
+  const [isSubmitting, setSubmitting] = useState(false);
 
+  useEffect(() => {
+    const fetchCustomDentalPlan = async () => {
+      try {
+        const res = await getCustomDentalPlan(user.$id);
+        setForm({
+          age: res.age || "",
+          existingConditions: res.existingConditions || "",
+          recentProcedures: res.recentProcedures || "",
+          hygieneFrequency: res.hygieneFrequency || "",
+          toothbrush: res.toothbrush || "",
+          toothpaste: res.toothpaste || "",
+          dietaryHabits: res.dietaryHabits || "",
+          dentalAppliances_allergies: res.dentalAppliances_allergies || "",
+        });
 
+      } catch (error) {
+        console.error(error);
+        Alert.alert("Error", "Failed to fetch dental plan");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCustomDentalPlan();
+  }, []);
 
   const submit = async () => {
     try {
       for (const key in form) {
-        if (key === 'age') {
-          // Check if age field is empty
-          if (form[key] === 0 || form[key] === '' || form[key] === undefined || form[key] === null) {
-            Alert.alert("Error", "Please enter a valid age");
-            return;
-          }
-        } else if (form[key].trim() === "") {
+        if (form[key].toString().trim() === "") {
           Alert.alert("Error", "Please fill in all fields");
           return;
         }
@@ -47,7 +56,6 @@ const DentalHistory = () => {
 
       setSubmitting(true);
       await updateCustomDentalPlan({ ...form, userId: user.$id, creator_dental: user.$id });
-      router.replace("/home");
     } catch (error) {
       console.error("Error submitting form:", error);
     } finally {
@@ -55,24 +63,25 @@ const DentalHistory = () => {
     }
   };
 
-
-
-
+  if (isLoading) {
+    return (
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView className="px-4 my-6">
+          <Text className="text-2xl text-white font-psemibold">Loading...</Text>
+          <Loader isLoading={isLoading}/>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView className="bg-primary h-full">
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <ScrollView className="px-4 my-6">
         <Text className="text-2xl text-white font-psemibold">Dental History</Text>
-        {/* 
-                title="Video Title"
-          value={form.title}
-          placeholder="Give your video a catchy title..."
-          handleChangeText={(e) => setForm({ ...form, title: e })}
-          otherStyles="mt-10" */}
-
         <DentalHistoryFormField
           title="Age"
-          value={form.age}
+          value={form.age.toString()}
           placeholder={"How old are you?"}
           handleChangeText={(e) => setForm({ ...form, age: e })}
           otherStyles={"mt-10"}
@@ -86,6 +95,7 @@ const DentalHistory = () => {
           handleChangeText={(e) => setForm({ ...form, existingConditions: e })}
           otherStyles={"mt-10"}
         />
+
 
         <DentalHistoryFormField
           title="Did you have any recent dental procedures or discomfort?"
@@ -127,28 +137,24 @@ const DentalHistory = () => {
           otherStyles={"mt-10"}
         />
 
-        {/* <DentalHistoryFormField
+        <DentalHistoryFormField
                     title="What dental appliances or allergies do you have?"
-                    value={"h"}
+                    value={form.dentalAppliances_allergies}
                     placeholder={"Please describe and type here"}
                     handleChangeText={(e) => setForm({ ...form, dentalAppliances_allergies: e })}
                     otherStyles={"mt-10"}
-                /> */}
+                />
 
-        < CustomButton
+        <CustomButton
           title="Save"
           handlePress={submit}
           containerStyles="mt-7"
           isLoading={isSubmitting}
         />
-
       </ScrollView>
-
-
-
+      </KeyboardAvoidingView>
     </SafeAreaView>
-  )
-
-}
+  );
+};
 
 export default DentalHistory;
